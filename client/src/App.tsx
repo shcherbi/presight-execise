4caa4df1736d6860a805bfed105bf3851622a816
Ilya Shcherbakov <ilya.shcherbakov@presight.ai>
2026-09-11 00:02:15 +0400

    Add FilterPanel component (design)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Header from "./components/Header/Header.tsx";
 import SearchBar from "./components/SearchBar/SearchBar.tsx";
+import FilterPanel from "./components/FilterPanel/FilterPanel.tsx";
 
 function App() {
     return (
-        <>
+        <div className={"wrapper-container"}>
             <Header/>
             <SearchBar/>
             {/*          <div>
@@ -12,53 +13,26 @@ function App() {
             <label>Diving X</label>
             <a>Remove All</a>
           </div>*/}
-            <aside>
-                <h2>
-                    Refine Records
-                </h2>
-                <div>
-                    <span>Hobbies</span>
-                    <span>TOP 20</span>
+            <div className={"main-container"}>
+                <FilterPanel/>
+                <main>
+                    <div>
+                        <h2> Verified Operatives</h2>
+                        <span>Matching case files</span>
+                        <span>240 records found</span>
+                    </div>
                     <div>
                         <div>
-                            <input type={"checkbox"} title={"Diving"}/> Diving
-                        </div>
-                        <div>
-                            <input type={"checkbox"} title={"Diving"}/> Boxing
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <span>Nationalities</span>
-                    <span>TOP 20</span>
-                    <div>
-                        <div>
-                            <input type={"checkbox"} title={"English"}/> English
-                        </div>
-                        <div>
-                            <input type={"checkbox"} title={"Indus"}/> Indus
+                            <svg></svg>
+                            <h1>John Doe</h1>
+                            <span>Polish</span>
+                            <span>46 old</span>
+                            <label>Diving</label>
                         </div>
                     </div>
-                </div>
-            </aside>
-            <main>
-                <div>
-                    <h2> Verified Operatives</h2>
-                    <span>Matching case files</span>
-                    <span>240 records found</span>
-                </div>
-                <div>
-                    <div>
-                        <svg></svg>
-                        <h1>John Doe</h1>
-                        <span>Polish</span>
-                        <span>46 old</span>
-                        <label>Diving</label>
-                    </div>
-                </div>
-
-            </main>
-        </>
+                </main>
+            </div>
+        </div>
     )
 }
 
---
+++ client/src/components/FilterPanel/FilterPanel.tsx
@@ -0,0 +1,57 @@
+import "./FilterPanel.css"
+
+function FilterPanel() {
+    return (
+        <aside className={"filter-panel"}>
+            <div className={"filter-panel-title"}>
+            <h2>
+                Refine Records
+            </h2>
+            <button>Clear all</button>
+            </div>
+
+            <div className={"filter-section"}>
+                <div className={"filter-heading"}>
+                    <h3>Hobbies</h3>
+                    <span>TOP 20</span>
+                </div>
+                <div>
+                    <div className={"filter-checkbox"}>
+                        <div>
+                            <input type={"checkbox"} title={"Diving"}/> Diving
+                        </div>
+                        <strong>20</strong>
+                    </div>
+                    <div className={"filter-checkbox"}>
+                        <div>
+                            <input type={"checkbox"} title={"Diving"}/> Boxing
+                        </div>
+                        <strong>10</strong>
+                    </div>
+                </div>
+            </div>
+            <div className={"filter-section"}>
+                <div className={"filter-heading"}>
+                    <h3>Nationalities</h3>
+                    <span>TOP 20</span>
+                </div>
+                <div>
+                    <div className={"filter-checkbox"}>
+                        <div>
+                            <input type={"checkbox"} title={"English"}/> English
+                        </div>
+                        <strong>20</strong>
+                    </div>
+                    <div className={"filter-checkbox"}>
+                        <div>
+                            <input type={"checkbox"} title={"Indus"}/> Indus
+                        </div>
+                        <strong>10</strong>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    );
+}
+
+export default FilterPanel;
