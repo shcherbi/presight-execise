1806089c9374fcf2551b83762871440b03a0c04a
Ilya Shcherbakov <ilya.shcherbakov@presight.ai>
2026-09-10 13:10:55 +0400

    Serve avatars on the server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import express, {type Express} from "express";
 
 import userRoutes from "./routes/userRoutes.ts";
+import {resolve} from "node:path";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use("/avatars", express.static(resolve(import.meta.dirname, "../db/avatars")));
 app.use("/users", userRoutes);
 
 export default app;
